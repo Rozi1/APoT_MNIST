f557b819c9354a249b6e21ea6d9f909a5432ea38
Rozi1 <rozi.kh2@gmail.com>
2020-11-23 16:26:07 +0500

    Update resnet.py

--- CIFAR10/models/resnet.py
+++ CIFAR10/models/resnet.py
@@ -105,7 +105,7 @@ class ResNet_Cifar(nn.Module):
         self.layer1 = self._make_layer(block, 16, layers[0], float=float)
         self.layer2 = self._make_layer(block, 32, layers[1], stride=2, float=float)
         self.layer3 = self._make_layer(block, 64, layers[2], stride=2, float=float)
-        self.avgpool = nn.AvgPool2d(8, stride=1)
+        """self.avgpool = nn.AvgPool2d(8, stride=1)"""
         self.fc = last_fc(64 * block.expansion, num_classes)
 
         for m in self.modules():
