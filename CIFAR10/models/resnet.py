a72175f2f831aa02b3ef90a9a56f09a2553b81de
Rozi1 <rozi.kh2@gmail.com>
2020-11-24 11:52:36 +0500

    Update resnet.py

--- CIFAR10/models/resnet.py
+++ CIFAR10/models/resnet.py
@@ -102,7 +102,7 @@ class Bottleneck(nn.Module):
 
 class ResNet(nn.Module):
 
-    def __init__(self, block, layers, num_classes=1000, float=false, zero_init_residual=False):
+    def __init__(self, block, layers, num_classes=1000, float=False, zero_init_residual=False):
         super(ResNet, self).__init__()
         self.inplanes = 64
         self.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3,
